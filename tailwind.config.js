23d61ad4202ad9cb17cba39115c561c77baea03e
Youngjae <2015fromjeki@gmail.com>
2024-10-09 00:32:57 +1100

    Feature Section

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      spacing: {
+        180: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
